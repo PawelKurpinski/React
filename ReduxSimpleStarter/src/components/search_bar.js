6a1611af7cdd4c62093e74cb4e7b5c9955845fdf
pawelkurpinski <pkurpinski@wp.pl>
2018-05-19 21:44:51 +0200

    React starter search bar

--- ReduxSimpleStarter/src/components/search_bar.js
+++ ReduxSimpleStarter/src/components/search_bar.js
@@ -1,11 +1,24 @@
 import React, {Component} from 'react';
 
 class SearchBar extends Component{
+    // render(){
+    //     return(
+    //     <input onChange = {this.onInputChange} />
+    //     )
+    // }
+    // onInputChange(event){  /* event and onInput...are my names */
+    //     return(
+    //         console.log(event.target.value)
+    //     )
+    // }; or much shorter syntax with arrow function!
+
     render(){
-    return(
-    <input />
-    )
+        return(
+            <input onChange = {event => console.log(event.target.value)} /> 
+        )
     }
+
+
 };
 
 export default SearchBar
