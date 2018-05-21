8505f1fd9b437e63030bb3b80b8e9fabe034d0b6
pawelkurpinski <pkurpinski@wp.pl>
2018-05-21 14:28:06 +0200

    save

--- ReduxSimpleStarter/src/components/search_bar.js
+++ ReduxSimpleStarter/src/components/search_bar.js
@@ -1,9 +1,13 @@
 import React, {Component} from 'react';
 
+/* What's going on? We want to show youtube films in our app. So we crated several components.
+Index.js is still (we will change it) a  functional component witch iports another components for example 'search_bar".
+A class base component can get props but functional comp not.   */
+
 class SearchBar extends Component{
     constructor(props){
         super(props)
-        this.state = { term: '' }; /* term is our name; we are initializing state */
+        this.state = { term: '' }; /* term is our name; we are initializing state - with empty string as it value for example "type sth" */
     }
     // render(){
     //     return(
@@ -25,7 +29,10 @@ class SearchBar extends Component{
             value = {this.state.term} /* we are actually updating user input when state changes */
             onChange = {event => this.setState({term: event.target.value})} /> 
            {/*  The value of input is: {this.state.term} */} {/* it is just a test we do not need to log it */}
-             {/* this looks strange but we want do it to get the value of the input from the page and update the state */}
+             {/* this looks strange but we want do it to get the value of the input from the page and update the state.
+            Here the key element is our input. It says our sate to update and rerender the page. \
+            We are using here next an CONTROLED COMPONENT - form element;
+            The value of the input is equal to the state */}
             </div>    
         )
     }
--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import SearchBar from './components/search_bar'
+import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
 
 const API_KEY = 'AIzaSyDrn9xRv8sLcxUifWnmvWcEetQNTcDRrSA';
 
-
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data){
+    console.log(data);
+})
 
 const App = () => {
     return (
