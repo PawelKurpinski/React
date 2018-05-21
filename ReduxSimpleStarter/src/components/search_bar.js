58472902ae23899f0ad02546464bbc5f52fea32c
pawelkurpinski <pkurpinski@wp.pl>
2018-05-21 12:56:30 +0200

    save

--- ReduxSimpleStarter/src/components/search_bar.js
+++ ReduxSimpleStarter/src/components/search_bar.js
@@ -24,7 +24,7 @@ class SearchBar extends Component{
             <input
             value = {this.state.term} /* we are actually updating user input when state changes */
             onChange = {event => this.setState({term: event.target.value})} /> 
-            {/* The value of input is: {this.state.term} */}
+            {/* The value of input is: {this.state.term}/* this looks strange but we want do is to get the value of the input from the page and update the state */}
             </div>    
         )
     }
