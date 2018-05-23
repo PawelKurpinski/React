a14c9d90295a33786ff4dec71fc2ab8418731a53
pawelkurpinski <pkurpinski@wp.pl>
2018-05-23 17:40:42 +0200

    save

---
+++ basicsOfReactTwo/style.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  'html': {
+    'display': 'flex',
+    'justifyContent': 'center'
+  },
+  'body': {
+    'display': 'flex',
+    'justifyContent': 'center'
+  },
+  'section#hello-world': {
+    'marginTop': [{ 'unit': 'px', 'value': 36 }],
+    'paddingBottom': [{ 'unit': 'px', 'value': 36 }],
+    'display': 'flex',
+    'alignItems': 'center',
+    'flexDirection': 'column',
+    'width': [{ 'unit': 'px', 'value': 500 }]
+  }
+});
