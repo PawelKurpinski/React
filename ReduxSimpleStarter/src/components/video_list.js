da37d811e2fe0d6156b579bbb40a36060f43e820
pawelkurpinski <pkurpinski@wp.pl>
2018-05-28 17:36:35 +0200

    save

--- ReduxSimpleStarter/src/components/video_list.js
+++ ReduxSimpleStarter/src/components/video_list.js
@@ -4,7 +4,7 @@ const VideoList = (props) => {
    /*  this function above is takig as argument props. Another words it's like: const videos = props.videos; */
     return(
         <ul className='col-md-4 list-group'>
-            {props.videos.length}
+            /* {props.videos.length} */
         </ul>
     );
 };
--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -13,8 +13,8 @@ class App extends Component {
         
         this.state = { videos: [] }; /* videos is the name that is up to us; we will get links, so this object will have an array inside of it */
        
-        YTSearch({key: API_KEY, term: 'surfboards'}, (data) => {
-            this.setState({ videos: data });
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+            this.setState({ videos });
         });
     }   
             /*YTSearch({key: API_KEY, term: 'surfboards'}, (data) => { this.setState({ videos: data }); }); It was like this but! when we have the same words as key and value we can delete one of them istead leving videos: videos; This name and value is OUR CHOICE!it's different videos than above */
@@ -22,7 +22,7 @@ class App extends Component {
             return(
             <div>
             <SearchBar />
-            <VideoList videos={this.state.videos} />
+            <VideoList videos={this.state.videos} /> {/* app is the parent of VideoList we do it with state but here we pass it as PROPS! */}
             </div>
         );
     }    
