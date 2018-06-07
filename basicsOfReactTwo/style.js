7a018dcf39e94438aa70d5641a9eeba8ed6704a1
pawelkurpinski <pkurpinski@wp.pl>
2018-06-07 10:48:17 +0200

    jQuery save

--- basicsOfReactTwo/style.js
+++ basicsOfReactTwo/style.js
@@ -3,18 +3,21 @@ import { StyleSheet } from 'react-native';
 export default StyleSheet.create({
   'html': {
     'display': 'flex',
-    'justifyContent': 'center'
+    'justifyContent': 'center',
+    'fontFamily': 'Arial, Helvetica, sans-serif'
   },
   'body': {
     'display': 'flex',
-    'justifyContent': 'center'
+    'justifyContent': 'center',
+    'fontFamily': 'Arial, Helvetica, sans-serif'
   },
   'section#hello-world': {
-    'marginTop': [{ 'unit': 'px', 'value': 36 }],
+    'marginTop': [{ 'unit': '%V', 'value': 0.3 }],
     'paddingBottom': [{ 'unit': 'px', 'value': 36 }],
     'display': 'flex',
     'alignItems': 'center',
     'flexDirection': 'column',
-    'width': [{ 'unit': 'px', 'value': 500 }]
+    'width': [{ 'unit': 'px', 'value': 800 }],
+    'borderRadius': '25%'
   }
 });
