62da89c8d9e341b0b41baa7793c764a65d4c47b4
pawelkurpinski <pkurpinski@wp.pl>
2018-06-12 17:19:55 +0200

    save

--- reactLandingPage/src/components/App.js
+++ reactLandingPage/src/components/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import Header from './Header/Header'
+import './reactLandingPage/App.css'
 
 class App extends Component {
 
   render() {
     return (
-      <div>
+      <div className="App">
         <Header />
       </div>
     );
