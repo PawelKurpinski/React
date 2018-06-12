a105ca4a79a1a30380c882d9be0d2f731ea37cfa
pawelkurpinski <pkurpinski@wp.pl>
2018-06-12 16:49:45 +0200

    save

--- reactLandingPage/src/components/Header/Header.js
+++ reactLandingPage/src/components/Header/Header.js
@@ -3,7 +3,9 @@ import React, {Component} from 'react';
 class Header extends Component{
     render(){
         return(
-            <h1>Hello!!</h1>
+            <div className="app">
+                Header!!
+            </div>
         )
 
     }
