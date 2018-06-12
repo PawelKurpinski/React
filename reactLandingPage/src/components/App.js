726a4e5bc06fa7411e27c3f005cb4a9b37e7f4c8
pawelkurpinski <pkurpinski@wp.pl>
2018-06-12 18:27:15 +0200

    reactLandingPage

--- reactLandingPage/src/components/App.js
+++ reactLandingPage/src/components/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
+import './App.css'
 import Header from './Header/Header';
 import Footer from './Footer/Footer';
-import './App.css'
 
 class App extends Component {
 
