de6bfbe09e9474e897832558e75de89a9cd6f726
pawelkurpinski <pkurpinski@wp.pl>
2018-06-13 16:58:10 +0200

    save

--- reactLandingPage/react-landing-page/src/Person/Person.jsx
+++ reactLandingPage/react-landing-page/src/Person/Person.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const person = () => {
     return (
-        <p>This is my new Person!</p>
+        <p>This is my new Person! His age is {Math.floor(Math.random()*30)}</p>
     )
 };
 
