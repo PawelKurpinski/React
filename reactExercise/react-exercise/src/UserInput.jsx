c984c2559f8cc7fd19b7804e72fa4be6afa22f79
Paweł <pkurpinski@wp.pl>
2018-06-16 22:02:10 +0200

    zapis

---
+++ reactExercise/react-exercise/src/UserInput.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+const userInput = () => {
+  return (
+    <div>
+      <input type="text" />
+    </div>
+  );
+};
+
+export default userInput;
---
+++ reactExercise/react-exercise/src/UserOutput.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+const userOutput = props => {
+  return (
+    <div>
+      <p>User name is:{props.userName} </p>
+      <p>Par 2</p>
+    </div>
+  );
+};
+
+export default userOutput;
